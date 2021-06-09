fb5e10b9f8d591ba9d71aafd2f123afa31e4c826
Ovais09 <ovazeem100@gmail.com>
2021-06-09 00:13:41 -0400

    refactored emptyBoard function to make it work withmns, also added another test for this which passed

--- lab04/tic_java/test/ticTest.java
+++ lab04/tic_java/test/ticTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert.*;
+
 import src.tic;
 
 
@@ -14,7 +18,13 @@ public class ticTest {
 
     @Test 
     void test (){
-        assertEquals("_", object.emptyBoard_1X1(1,1));
-    }
+        // 1 X 1
+        // assertEquals("_", object.emptyBoard(1,1));
+
+        // 2 X 2 
+        String [][] result = {{"_","_"}, {"_","_"}};
+        assertArrayEquals(result,object.emptyBoard(2,2));
     
+}
+
 }
