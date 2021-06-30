b185ed9b4c0e84877396fe2e9350740eca727d2b
Ovais09 <ovazeem100@gmail.com>
2021-06-29 21:00:44 -0400

    created assets folder and README and put images in the assets folder and implemented the functions in grades.ex

--- lab05/grades/grades/lib/grades.ex
+++ lab05/grades/grades/lib/grades.ex
@@ -1,4 +1,4 @@
-defmodule Grades do
+defmodule Grades.Calculator do
   @moduledoc """
   Grades keeps the contexts that define your domain
   and business logic.
@@ -6,4 +6,19 @@ defmodule Grades do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def letter_grade (%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    list = ["A+", "A", "A-", "B+", "B", "C+", "C", "D+", "D", "E", "F"]
+    Enum.take_random(list,1)
+  end
+
+  def percentage_grade (%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    Enum.random(0..100)
+  end
+
+  def numeric_grade (%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    Enum.random(0..10)
+  end
+
+
 end
